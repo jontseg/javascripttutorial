0fb9a55760d32a4218d2ac96888d38d15ad21c77
jontseg <jontseg@gmail.com>
2025-03-30 11:19:44 -0700

    first part

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,93 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+var country = "US";
+var continent = "NA";
+let population = 230;
+
+console.log(country, continent, population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof country, typeof population, typeof language);
+
+language = "bing bong";
+
+console.log(population / 2);
+console.log(population + 1);
+console.log(population > 6);
+console.log(population < 33);
+
+let description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+
+console.log(description);
+
+description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(description);
+
+population = 13;
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(`${country}'s population is ${33 - population} below average`);
+}
+
+console.log("9" - "5"); // ->  4
+console.log("19" - "13" + "17"); // -> 23
+console.log("19" - "13" + 17); // -> 23
+console.log("123" < 57); // -> false
+console.log(5 + 6 + "4" + 9 - 4 - 2); // -> number
+
+// numNeighbors = prompt("How many neighbor countreis does your country have");
+// numNeighbors = Number(numNeighbors)
+// if (numNeighbors === 1) {
+//   console.log(`Only ${numNeighbors} border`);
+// } else if (numNeighbors > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("No borders");
+// }
+
+if (language == "English" && population < 50 && !isIsland) {
+    console.log(`You should live in ${country}`)
+} else{
+    console.log(`${country} does not meet your criteria :(`)
+}
+
+
+switch(language.toLowerCase()) {
+    case "chinese":
+    case "mandarin":
+        console.log("MOST number of native speakers!")
+        break
+    case "spanish":
+        console.log("2nd place in number of native speakers")
+        break
+    case "english":
+        console.log("3rd place")
+        break
+    case "hindi":
+        console.log("Number 4")
+        break
+    case "arabic":
+        console.log("5th most spoken language")
+        break
+    default:
+        console.log("Great language too :D")
+
+    
+}
+
+
+console.log(
+    `${country}'s population is ${population > 33 ? 'above' : 'below'} average`
+)
