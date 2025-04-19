1d8238ba2a05be050f7d3d431d312633be637649
jontseg <jontseg@gmail.com>
2025-04-19 12:09:33 -0700

    clean

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -14,17 +14,54 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 
+// const workout = `<h2 class="workout__title">Running on April 14</h2>
+//           <div class="workout__details">
+//             <span class="workout__icon">🏃‍♂️</span>
+//             <span class="workout__value">5.2</span>
+//             <span class="workout__unit">km</span>
+//           </div>
+//           <div class="workout__details">
+//             <span class="workout__icon">⏱</span>
+//             <span class="workout__value">24</span>
+//             <span class="workout__unit">min</span>
+//           </div>
+//           <div class="workout__details">
+//             <span class="workout__icon">⚡️</span>
+//             <span class="workout__value">4.6</span>
+//             <span class="workout__unit">min/km</span>
+//           </div>
+//           <div class="workout__details">
+//             <span class="workout__icon">🦶🏼</span>
+//             <span class="workout__value">178</span>
+//             <span class="workout__unit">spm</span>
+//           </div>
+//         </li>`
+
+
+
 navigator.geolocation.getCurrentPosition(function(position) {
   const {latitude, longitude} = position.coords;
   console.log(latitude, longitude);
   console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoASAFQAw%3D%3D`);
-  const map = L.map('map').setView([latitude, longitude], 13);
+  const coords = [latitude, longitude];
+  const map = L.map('map').setView(coords, 13);
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // map.on('click', renderWorkout())
+  // function onMapClick(e) {
+  //     alert("You clicked the map at " + e.latlng);
+  // }
+  // function renderWorkout(e) {
+  //   form.classList.toggle('hidden');
+  // }
+
+  // map.on('click', renderWorkout);
+
+
+  L.tileLayer('https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/">Humanitarian OpenStreetMap Team</a>'
   }).addTo(map);
 
-  L.marker([latitude, longitude]).addTo(map)
+  L.marker(coords).addTo(map)
   .bindPopup('A Pretty CSS popup.<br> Easily customizable.')
   .openPopup();
 }, function() {
